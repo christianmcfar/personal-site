60b833bb47f591c837f42995889214275ad9f00e
Christian <christianmcfa@gmail.com>
2023-12-21 15:43:10 -0500

    added scroll snaping and additional text sections

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { loadFull } from "tsparticles";
 import "./App.css";
 import particlesOptions from "./particles.json";
 import { FaGithub, FaLinkedinIn, FaMapMarkedAlt } from "react-icons/fa";
-import Resume from "./Christian McFarland - Resume.pdf"
-import { TypeAnimation } from 'react-type-animation'
+import Resume from "./Christian McFarland - Resume.pdf";
+import { TypeAnimation } from 'react-type-animation';
 
 function App() {
     const [init, setInit] = useState(false);
@@ -24,64 +24,87 @@ function App() {
 
     return (
         <div className="App">
-            <nav className='Nav'>
-                <a href="https://github.com/christianmcfar" target="_blank" rel="noopener noreferrer">
-                    <FaGithub className='Nav-item' size={25} />
-                </a>
-                <a href="https://www.linkedin.com/in/christian-mcfarland/" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedinIn className='Nav-item' size={25} />
-                </a>
-                <a className="tooltip">
-                    <FaMapMarkedAlt className='Nav-item' size={25} />
-                    <span class="tooltiptext">Coming soon</span>
-                </a>
-            </nav>
             {init && <Particles options={particlesOptions} />}
-            <header className="App-header">
-                <h1 className='Title-align'>
-                    Christian McFarland
-                </h1>
-                <TypeAnimation
-                    className='Title-align'
-                    sequence={[
-                        'Software Engineer',
-                        4000,
-                        'Front End Developer',
-                        4000,
-                        'Motorcycle Instructor',
-                        2000,
-                        'Mobile App Devloper',
-                        3000,
-                        'Video Editor',
-                        2000,
-                        'Hiking Enthusiast',
-                        3000,
-                        'Software Developer',
-                        4000,
-                        'Marathon Runner',
-                        2000,
-                        'Fantasy Football Champion',
-                        1000,
-                        'Scuba Diver',
-                        1000
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    style={{ fontSize: 'larger', display: 'inline-block' }}
-                    repeat={Infinity}
-                />
-                <p className="Title-line">
-                    Check out my <a
-                        className="App-link"
-                        href={Resume}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <b>resume</b>
-                    </a> here!
-                </p>
-                
-            </header>
+            <body>
+                <div className='wrapper'>
+                    <section>
+                        <nav className='Nav'>
+                            <a href="https://github.com/christianmcfar" target="_blank" rel="noopener noreferrer">
+                                <FaGithub className='Nav-item' size={25} />
+                            </a>
+                            <a href="https://www.linkedin.com/in/christian-mcfarland/" target="_blank" rel="noopener noreferrer">
+                                <FaLinkedinIn className='Nav-item' size={25} />
+                            </a>
+                            <div className="tooltip">
+                                <FaMapMarkedAlt className='Nav-item' size={25} />
+                                <span class="tooltiptext">Map coming soon!</span>
+                            </div>
+                        </nav>
+                        <div className='App-header'>
+                            <h1 className='Title-align'>
+                                Christian McFarland
+                            </h1>
+                            <TypeAnimation
+                                className='Title-align'
+                                sequence={[
+                                    1000,
+                                    'Software Engineer',
+                                    4000,
+                                    'Front End Developer',
+                                    4000,
+                                    'Motorcycle Instructor',
+                                    2000,
+                                    'Mobile App Devloper',
+                                    3000,
+                                    'Video Editor',
+                                    2000,
+                                    'Hiking Enthusiast',
+                                    3000,
+                                    'Software Developer',
+                                    4000,
+                                    'Marathon Runner',
+                                    2000,
+                                    'Fantasy Football Champion',
+                                    1000,
+                                    'Scuba Diver',
+                                    500
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ fontSize: 'xxx-large', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
+                        </div>
+                    </section>
+                    <section className="textSection">
+                        <div className="content">
+                            <h2>
+                                Welcome to my personal website!
+                            </h2>
+                            <p>
+                                Here you'll find my resume and some social media links, as well as some other personal 
+                                projects as I build them. It's a simple site created using React, tsParticles for the background effects, some 
+                                CSS for styling compements, and Vercel for deployment and hosting.
+                            </p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className='App-header'>
+                            <h2 className='Title-align'>
+                                More coming soon!
+                            </h2>
+                            <p className="Title-line">
+                                But for now, why not check out my <a
+                                    className="App-link"
+                                    href={Resume}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >resume</a>?
+                            </p>
+                        </div>
+                    </section>
+                </div>
+            </body>
         </div>
     );
 }
